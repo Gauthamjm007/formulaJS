8d01cb1d5ae5e2a6b66d60b318462093eb531d75
Gauthamjm007 <j.mgautham15@gmail.com>
2020-07-20 02:55:23 +0530

    adding perimeter and area of a ellipse

--- geometry/shapes.js
+++ geometry/shapes.js
@@ -24,29 +24,43 @@ const shapes = {
     },
   },
   triangle: {
-    area: function (b, h) {
+    area: function (b = 0, h = 0) {
       return ((1 / 2) * b * h).toFixed(2);
     },
-    perimeter: function (a, b, c) {
+    perimeter: function (a = 0, b = 0, c = 0) {
       return (a + b + c).toFixed(2);
     },
   },
   parallelogram: {
-    area: function (b, h) {
+    area: function (b = 0, h = 0) {
       return (b * h).toFixed(2);
     },
-    perimeter: function (b, h) {
+    perimeter: function (b = 0, h = 0) {
       return (2 * (b + h)).toFixed(2);
     },
   },
   trapezoid: {
-    area: function (h, b1, b2) {
+    area: function (h = 0, b1 = 0, b2 = 0) {
       return (((b1 + b2) / 2) * h).toFixed(2);
     },
-    perimeter: function (a, b, c, d) {
+    perimeter: function (a = 0, b = 0, c = 0, d = 0) {
       return (a + b + c + d).toFixed(2);
+    },
+  },
+  ellipse: {
+    area: function (r1 = 0, r2 = 0) {
+      return Math.PI.toFixed(2) * r1 * r2;
+    },
+    perimeter: function (r1 = 0, r2 = 0) {
+      return (
+        2 *
+        Math.PI.toFixed(2) *
+        Math.sqrt(
+          Math.pow(r1, 2).toFixed(2) + Math.pow(r2, 2).toFixed(2) / 2
+        ).toFixed(2)
+      ).toFixed(2);
     },
   },
 };
 
-console.log(shapes.circle.circumference(4));
+console.log(shapes.ellipse.perimeter(4, 4));
