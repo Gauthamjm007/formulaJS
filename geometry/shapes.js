32a2922a0c22f1f2094cbfbdccc8d72328f0aa95
Gauthamjm007 <j.mgautham15@gmail.com>
2020-07-20 02:32:43 +0530

    adding area of a rectange and circle

--- geometry/shapes.js
+++ geometry/shapes.js
@@ -7,6 +7,22 @@ const shapes = {
       return 4 * a;
     },
   },
+  rectangle: {
+    area: function (a = 0, b = 0) {
+      return (a * b).toFixed(2);
+    },
+    perimeter: function (a = 0, b = 0) {
+      return (2 * (a + b)).toFixed(2);
+    },
+  },
+  circle: {
+    area: function (r = 0) {
+      return (Math.PI.toFixed(2) * r * r).toFixed(2);
+    },
+    circumference: function (r = 0) {
+      return (2 * Math.PI * r).toFixed(2);
+    },
+  },
 };
 
-console.log(shapes.square.perimeter(4));
+console.log(shapes.circle.circumference(4));
