a5087d0410ded023a184f37babff72c2ee1261b7
Gauthamjm007 <j.mgautham15@gmail.com>
2020-07-20 02:44:47 +0530

    adding for triangle , parllelogram , trapezoid

--- geometry/shapes.js
+++ geometry/shapes.js
@@ -23,6 +23,30 @@ const shapes = {
       return (2 * Math.PI * r).toFixed(2);
     },
   },
+  triangle: {
+    area: function (b, h) {
+      return ((1 / 2) * b * h).toFixed(2);
+    },
+    perimeter: function (a, b, c) {
+      return (a + b + c).toFixed(2);
+    },
+  },
+  parallelogram: {
+    area: function (b, h) {
+      return (b * h).toFixed(2);
+    },
+    perimeter: function (b, h) {
+      return (2 * (b + h)).toFixed(2);
+    },
+  },
+  trapezoid: {
+    area: function (h, b1, b2) {
+      return (((b1 + b2) / 2) * h).toFixed(2);
+    },
+    perimeter: function (a, b, c, d) {
+      return (a + b + c + d).toFixed(2);
+    },
+  },
 };
 
 console.log(shapes.circle.circumference(4));
