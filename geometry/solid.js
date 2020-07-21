9ee81fc041a7e3241ecc471934cca062c6236628
Gauthamjm007 <j.mgautham15@gmail.com>
2020-07-21 23:57:30 +0530

    adding sphere formulas

--- geometry/solid.js
+++ geometry/solid.js
@@ -7,4 +7,12 @@ const solid = {
       return Math.pow(a, 3);
     },
   },
+  sphere: {
+    surfaceArea: function (r) {
+      return 4 * Math.PI * r * r;
+    },
+    volume: function (r) {
+      return (4 / 3) * Math.PI * Math.pow(r, 3);
+    },
+  },
 };
