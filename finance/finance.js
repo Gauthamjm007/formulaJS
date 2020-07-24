97b994ef56bcd08e8bd36e2238222722b3aa00d0
Gauthamjm007 <j.mgautham15@gmail.com>
2020-07-24 23:50:26 +0530

    adding emi

--- finance/finance.js
+++ finance/finance.js
@@ -15,4 +15,13 @@ const finance = {
       newAmount: roundTo(interest, 2),
     };
   },
+  emi: function (principal = 0, timePeriod = 0, annual_rate = 12) {
+    annual_rate = annual_rate / (12 * 100);
+    const emi =
+      (principal * annual_rate * Math.pow(annual_rate + 1, timePeriod)) /
+      (Math.pow(1 + annual_rate, timePeriod) - 1);
+    return roundTo(emi, 2);
+  },
 };
+
+// console.log(EMICalculator(1000, 10, 6));
