6388b0c9ce00ef07ef3340692fb9c322b891e110
Gauthamjm007 <j.mgautham15@gmail.com>
2020-07-28 00:06:38 +0530

    adding multiplication of a matrix

--- globalFunctions.js
+++ globalFunctions.js
@@ -17,4 +17,26 @@ const roundTo = (num, digits) => {
   return Number(num);
 };
 
-module.exports = { roundTo };
+const matrixMultiplication = (A = [1], B = [1]) => {
+  const matrices = [A, B];
+  const cols = matrices.map((item) => item[0].length);
+  if (!matrices.every((item, i) => item.every((row) => row.length === cols[i])))
+    return console.error(
+      "All rows in a matrix must have equal amount of columns"
+    );
+  else if (cols[0] !== B.length)
+    return console.error(
+      "Amount of columns in the 1st matrix must match amount of rows in the 2nd matrix"
+    );
+
+  return A.map((rowA) =>
+    B[0].map((_, colBIndex) =>
+      rowA.reduce(
+        (acc, itemA, rowBIndex) => acc + itemA * B[rowBIndex][colBIndex],
+        0
+      )
+    )
+  );
+};
+
+module.exports = { roundTo, matrixMultiplication };
